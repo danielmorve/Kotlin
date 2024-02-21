2f714adb5a87bd8007a819e7124f79c55effd775
danielmorve <danimove1@hotmail.com>
2024-02-21 15:05:17 -0600

    FoodApp

---
+++ app/src/main/java/com/example/foodapp/BuyActivity.kt
@@ -0,0 +1,10 @@
+package com.example.foodapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+class BuyActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_compra)
+    }
+}
